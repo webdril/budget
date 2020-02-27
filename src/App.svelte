b8ce66c792c012ae3b83cc0317dedfd65a4dbf2c
webdril <jimbahamas03@yahoo.com>
2020-02-27 20:32:41 +0000

    created a nav bar

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<!-- <script></script> -->
+<!-- Javascript Logic -->
+<!-- <style></style> -->
+<!-- CSS/STYLING -->
+<!-- html -->
+<nav class="nav">
+	<div class="nav-center">
+		<h1 class="nav-title">Budget Calculator</h1>
+		<button type="button" class="nav-btn">
+			<i class="far fa-plus-square" />
+			Add Item
+		</button>
+	</div>
+</nav>
